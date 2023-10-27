5f4d903a5f91008da473b8f2e9a209367d0aba78
cs-ankit-gupta <gankit@fortinet.com>
2023-10-27 14:46:40 +0530

    Added logic to add new version

--- documenting/automation/solution-pack/SP-Doc-Automation/main.py
+++ documenting/automation/solution-pack/SP-Doc-Automation/main.py
@@ -6,140 +6,189 @@ import zipfile
 from release_note import *
 import logging
 import argparse
+from urllib.parse import urlparse
+import sys
+from utils import *
 
 
 class SolutionPackDocsAutomation:
 
-    def __init__(self, sp_path):
-        self.sp_path = str(sp_path)
+    def __init__(self, sp_dir_path, sp_repo_name, old_version_branch_name, is_new_version):
+        self.sp_dir_path = str(sp_dir_path)
+        self.is_new_version = is_new_version
+        self.sp_repo_name, self.old_version_branch_name = '', ''
+        if sp_repo_name:
+            self.sp_repo_name = sp_repo_name[0]
+        if old_version_branch_name:
+            self.old_version_branch_name = old_version_branch_name[0]
 
     def create_docs(self):
         try:
-            if not os.path.exists(self.sp_path):
-                raise Exception(
-                    'Provided Solution Pack path \"{0}\" does not exists. Please provide proper path.'.format(self.sp_path))
-            if os.path.isdir(self.sp_path):
-                logging.debug(
-                    'Provided Solution Pack path: \"{0}\" is a directory.'.format(self.sp_path))
-                sp_dir_path = self.sp_path
-            elif zipfile.is_zipfile(self.sp_path):
-                logging.debug(
-                    'Provided Solution Pack path: \"{0}\" is a zip file.'.format(self.sp_path))
-                sp_dir_path = self.unzip_sp_zip_folder()
-                logging.debug('Successfully unzip Solution Pack')
-            else:
-                raise Exception(
-                    "Solution Pack should be either folder or zip.")
-
-            sp_file_folder_list = os.listdir(sp_dir_path)
+            sp_file_folder_list = os.listdir(self.sp_dir_path)
             if "info.json" not in sp_file_folder_list:
-                raise Exception(
-                    "Solution Pack does not contain 'info.json' file under path: \"{0}\". Please export the Solution Pack once again.".format(sp_dir_path))
+                raise Exception(f"Solution Pack does not contain 'info.json' file under path: {self.sp_dir_path}. Please export the Solution Pack once again.")
 
             # Get info.json data
-            info_json_data = self.get_info_json_data(sp_dir_path)
+            info_json_data = self.get_info_json_data(self.sp_dir_path)
             logging.debug("Successfully read info.json file")
 
-            # check provided path is Solution Pack or not:
+            # check provided path is Solution Pack or not
             if info_json_data.get('type') != 'solutionpack':
-                raise Exception(
-                    "Provide folder path is not of Solution Pack type.")
+                raise Exception("Provide folder path is not of Solution Pack type.")
 
-            sp_docs_path, sp_docs_res_path = self.create_docs_folder(
-                sp_dir_path)
+            # Check if docs folder and files exists or not
+            sp_docs_path, sp_docs_res_path = self.create_docs_folder(self.sp_dir_path)
 
             # Writing in Readme.md file
-            readme = Readme(sp_dir_path, info_json_data)
+            readme = Readme(self.sp_dir_path, info_json_data,self.is_new_version)
             readme.create_readme_file_data()
 
-            # Writing in Content.md file
-            contents = Contents(
-                sp_dir_path, sp_docs_path, sp_file_folder_list, info_json_data)
+            # # Writing in Content.md file
+            old_info_json = ''
+            if self.is_new_version:
+                old_info_json = self.get_old_info_json_data()
+            contents = Contents(self.sp_dir_path, sp_docs_path, sp_file_folder_list, info_json_data, self.is_new_version, old_info_json, self.sp_repo_name, self.old_version_branch_name)
             contents.create_contents_file_data()
 
             # Writing in Setup.md file
-            setup = Setup(sp_dir_path, sp_docs_path, sp_file_folder_list,
-                          info_json_data)
+            setup = Setup(self.sp_dir_path, sp_docs_path, sp_file_folder_list, info_json_data)
             setup.create_setup_file_data()
 
             # Writing in Usage.md file
-            usage = Usage(sp_dir_path, sp_docs_path,
-                          info_json_data)
-            usage.create_usage_file_data()
+            if not self.is_new_version:
+                usage = Usage(self.sp_dir_path, sp_docs_path, info_json_data)
+                usage.create_usage_file_data()
+            
+            logging.debug('Successfully created doc for Solution Pack \"' + info_json_data['label'] + '\" under provided path: {}'.format(self.sp_dir_path))
 
         except Exception as error:
             logging.exception(error)
+            sys.exit(1)
 
-    def unzip_sp_zip_folder(self):
-        with zipfile.ZipFile(self.sp_path, 'r') as sp_unzip_path:
-            sp_unzip_path.extractall(
-                path=os.path.dirname(sp_unzip_path.filename))
-            unzipped_folder_path = os.path.join(os.path.dirname(sp_unzip_path.filename),
-                                                os.path.splitext(os.path.basename(self.sp_path))[0])
-        return unzipped_folder_path
-        # with ZipFile(self.sp_path, 'r') as zip_ref:
-        #     # Extract all contents to the same directory as the zip file
-        #     extract_path = os.path.dirname(zip_ref.filename)
-        #     zip_ref.extractall(path=extract_path)
-        #
-        #     # Get the expected directory name
-        #     base_name = os.path.splitext(os.path.basename(self.sp_path))[0]
-        #     expected_dir_name = base_name.split(' ')[0]
-        #     print('expected_dir_name ', expected_dir_name)
-        #
-        #     # Check if the expected directory exists
-        #     extracted_dir = os.path.join(extract_path, expected_dir_name)
-        #     if not os.path.exists(extracted_dir):
-        #         # Rename the extracted directory to the expected name
-        #         os.rename(os.path.join(extract_path, base_name), extracted_dir)
-        #
-        #     # Return the path of the renamed directory
-        #     return extracted_dir
+    # Get old info.json file data
+    def get_old_info_json_data(self):
+        response = requests.get(OLD_SP_INFO_JSON_DATA.format(sp_repo_name=self.sp_repo_name, branch_name=self.old_version_branch_name))
+        info_json = json.loads(response.text)
+        return info_json
 
     # Get info.json file data
     def get_info_json_data(self, sp_dir_path):
         info_json_file_path = sp_dir_path + '/info.json'
         info_json_data = open(info_json_file_path)
         info_json = json.load(info_json_data)
+        info_json_data.close()
         return info_json
 
     # create doc folder if not exists
     def create_docs_folder(self, sp_dir_path):
         sp_docs_path = os.path.join(sp_dir_path, DOC_FOLDER_NAME)
         sp_docs_res_path = os.path.join(sp_docs_path, RESOURCE_FOLDER_NAME)
-        if not os.path.isdir(sp_docs_path):
-            os.mkdir(sp_docs_path)
-            logging.debug(
-                "Successfully created \"{0}\" folder.".format(DOC_FOLDER_NAME))
-        if not os.path.isdir(sp_docs_res_path):
-            os.mkdir(sp_docs_res_path)
-            logging.debug("Successfully created \"{0}\" folder under \"{1}\"".format(
-                RESOURCE_FOLDER_NAME, DOC_FOLDER_NAME))
+        for folder_path in [sp_docs_path, sp_docs_res_path]:
+            if not os.path.isdir(folder_path):
+                os.mkdir(folder_path)
+                logging.debug(f"Successfully created {folder_path} folder.")
+        if self.is_new_version:
+            self.download_and_create_md_files(OLD_SP_DOC_DATA.format(sp_repo_name=self.sp_repo_name, folder_to_copy='', branch_name=self.old_version_branch_name), sp_dir_path, False)
+            self.download_and_create_md_files(OLD_SP_DOC_DATA.format(sp_repo_name=self.sp_repo_name, folder_to_copy='docs', branch_name=self.old_version_branch_name), sp_docs_path, False)
+            self.download_and_create_md_files(OLD_SP_DOC_DATA.format(sp_repo_name=self.sp_repo_name, folder_to_copy='docs/res', branch_name=self.old_version_branch_name), sp_docs_res_path, True)
         return sp_docs_path, sp_docs_res_path
 
+    def download_and_create_md_files(self, url, base_folder, is_res_folder):
+        response = requests.get(url)
 
-def main():
+        if response.status_code == 200:
+            contents = response.json()
+
+            for item in contents:
+                if item['type'] == 'file':
+                    item_url = item['download_url']
+                    file_name = os.path.join(base_folder, item['name'])
+
+                    # Check if the file is an image (based on file extension)
+                    if is_res_folder and item['name'].lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                        file_content = requests.get(item_url).content
+                        with open(file_name, 'wb') as file:
+                            file.write(file_content)
+                        logging.debug(f"Downloaded image: {file_name}")
+
+                    # Check if the file is a markdown (.md) file
+                    elif not is_res_folder and item['name'].endswith('.md'):
+                        file_content = requests.get(item_url).text
+                        with open(file_name, 'w', encoding='utf-8') as file:
+                            file.write(file_content)
+                        logging.debug(f"Downloaded and created: {file_name}")
+        else:
+            raise Exception(f"Failed to fetch contents from {url}. Status code: {response.status_code}")
+
+
+# Get Solution Pack details
+def get_sp_details():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--sp-path', nargs='+', help='Path of Solution Pack', required=True)
+    parser.add_argument('--sp-repo-name', nargs=1, help='Provide the github Solution Pack repo name', required=False)
+    parser.add_argument('--old-version-branch-name', nargs=1, help='Provide the github old version branch name of Solution Pack', required=False)
+    return parser.parse_args()
 
-    parser.add_argument('--sp-path', nargs='+',
-                        help='Path of Solution Pack', required=True)
-    args = parser.parse_args()
-    docs_obj = SolutionPackDocsAutomation(' '.join(args.sp_path))
-    docs_obj.create_docs()
+# validate argument data
+def is_argument_data_valid(args):
+
+    try:
+        # validate solution pack provided path
+        sp_path = ' '.join(args.sp_path)
+        sp_valid_path = is_sp_path_valid(sp_path)
+        if sp_valid_path[0]:
+            sp_path = sp_valid_path[1]
+
+        # validate repo name and branch name
+        if args.sp_repo_name or args.old_version_branch_name:
+            if args.sp_repo_name != None and args.old_version_branch_name == None:
+                raise Exception(f'Solution Pack repository is specified as \"{args.sp_repo_name[0]}\", to create docs we requires the old version\'s branch name via the "--old-version-branch-name" argument.')
+            if args.old_version_branch_name != None and args.sp_repo_name == None:
+                raise Exception(f'Solution Pack old version\'s branch name is specified as \"{args.old_version_branch_name[0]}\", to create docs we requires the repository name via the "--sp-repo-name" argument.')
+            if args.sp_repo_name or args.old_version_branch_name:
+                response = requests.get(url=OLD_SP_INFO_JSON_DATA.format(sp_repo_name=args.sp_repo_name[0], branch_name=args.old_version_branch_name[0]))
+                if response.status_code != 200:
+                    raise Exception(f'Provided repository name: \"{args.sp_repo_name[0]}\", or branch name: \"{args.old_version_branch_name[0]}\" is incorrect.')
+        return sp_path
+    except Exception as error:
+        logging.exception(error)
+        sys.exit(1)
+
+def is_sp_path_valid(sp_path):
+    if not os.path.exists(sp_path):
+        raise Exception(f'Provided Solution Pack path {sp_path} does not exists. Please provide proper path.')
+    if os.path.isdir(sp_path):
+        logging.debug(f'Provided Solution Pack path:{sp_path} is a directory.')
+        return True, sp_path
+    elif zipfile.is_zipfile(sp_path):
+        logging.debug(f'Provided Solution Pack path: {sp_path} is a zip file.')
+        utils = Utility(sp_path)
+        return True, utils.unzip_zip_folder()
+    else:
+        raise Exception(f" Not a directory: \'{sp_path}\'. Solution Pack should be either \"folder\" or \"zip\".")
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.NOTSET, filename='docs.log', filemode='a',
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+def create_log_file():
+    log_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'docs.log')
+    logging.basicConfig(level=logging.NOTSET, filename=log_filename, filemode='a',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     # Create a console logger
     console_logger = logging.StreamHandler()
     console_logger.setLevel(logging.DEBUG)
-    console_formatter = logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(message)s")
+    console_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     console_logger.setFormatter(console_formatter)
 
     # Get the root logger
     logger = logging.getLogger()
     logger.addHandler(console_logger)
-    main()
+
+
+if __name__ == "__main__":
+    args = get_sp_details()
+    create_log_file()
+    is_new_version = False
+    sp_dir_path = is_argument_data_valid(args)
+    if args.sp_repo_name != None:
+        is_new_version = True
+    docs_obj = SolutionPackDocsAutomation(sp_dir_path, args.sp_repo_name, args.old_version_branch_name, is_new_version)
+    docs_obj.create_docs()
--- documenting/automation/solution-pack/SP-Doc-Automation/template.py
+++ documenting/automation/solution-pack/SP-Doc-Automation/template.py
@@ -1,74 +1,70 @@
 from string import Template
 
 # readme
-readme = Template(
-    '# Release Information \n\n * **Version**: $version \n * **Certified**: $certify \n * **Publisher**: $publisher \n * **Compatible Version**: $compatible_version \n$release_note \n\n # Overview \n $overview \n\n # Next Steps\n | [Installation](./$docs_name/setup.md#installation) | [Configuration](./$docs_name/setup.md#configuration) | [Usage](./$docs_name/usage.md) | [Contents](./$docs_name/contents.md) | \n |--------------------------------------------|----------------------------------------------|------------------------|------------------------------|')
+version_data = Template('- **Version**: $version')
+certified_data = Template('- **Certified**: $certify')
+compatible_version_data = Template('- **Compatible Version**: FortiSOAR $compatible_version')
+readme = Template('# Release Information \n\n$version_data \n$certified_data \n- **Publisher**: $publisher \n$compatible_version_data \n$release_note \n\n # Overview \n $overview \n\n # Next Steps\n | [Installation](./$docs_name/setup.md#installation) | [Configuration](./$docs_name/setup.md#configuration) | [Usage](./$docs_name/usage.md) | [Contents](./$docs_name/contents.md) | \n |--------------------------------------------|----------------------------------------------|------------------------|------------------------------|')
+
+
+# dashboard, modules, global variables, roles, report, widget, rule
+name_description_data = Template('| $name | $description |')
+
+# picklist, navigations
+name_data = Template('| $name |')
 
 # dashboard
-dashboards_sub_data = Template('| $name | $description |')
-dashboards_data = Template(
-    '## Dashboards\n\n|Name|Description|\n| :- | :- |\n$dashboards_sub_data')
+# dashboards_sub_data = Template('| $name | $description |')
+dashboards_data = Template('## Dashboards\n\n|Name|Description|\n| :- | :- |\n$dashboards_sub_data')
 
 # picklist
-picklists_sub_data = Template('| $name |')
-picklists_data = Template(
-    '## Picklist\n\n|Name|\n| :- |\n$picklists_sub_data')
+# picklists_sub_data = Template('| $name |')
+picklists_data = Template('## Picklist\n\n|Name|\n| :- |\n$picklists_sub_data')
 
 # modules
-modules_sub_data = Template('| $name | $description |')
-modules_data = Template(
-    '## Module Schema\n\n|Name|Description|\n| :- | :- |\n$modules_sub_data')
+# modules_sub_data = Template('| $name | $description |')
+modules_data = Template('## Module Schema\n\n|Name|Description|\n| :- | :- |\n$modules_sub_data')
 
 # global variables
-global_variables_sub_data = Template('| $name | $description |')
-global_variables_data = Template(
-    '## Global Variable\n\n|Name|Description|\n| :- | :- |\n$global_variables_sub_data')
+# global_variables_sub_data = Template('| $name | $description |')
+global_variables_data = Template('## Global Variable\n\n|Name|Description|\n| :- | :- |\n$global_variables_sub_data')
 
 # roles
-roles_sub_data = Template('| $name | $description |')
-roles_data = Template(
-    '## Roles\n\n|Name|Description|\n| :- | :- |\n$roles_sub_data')
+# roles_sub_data = Template('| $name | $description |')
+roles_data = Template('## Roles\n\n|Name|Description|\n| :- | :- |\n$roles_sub_data')
 
 # report
-report_data = Template('| $name | $description |')
-report_head = Template(
-    '## Report\n\n| Name | Description |\n| :- | :- |\n$report_data')
+# report_sub_data = Template('| $name | $description |')
+report_head = Template('## Report\n\n| Name | Description |\n| :- | :- |\n$report_sub_data')
 
 # record set
 record_sets_sub_data = Template('| $data1 | $data2 |')
-record_sets_data = Template(
-    '## $record_name Record set \n\n| Name | Description |\n| :- | :- |\n$record_sets_sub_data')
+record_sets_data = Template('## $record_name Record set \n\n| Name | Description |\n| :- | :- |\n$record_sets_sub_data')
 
 # connectors
 connectors_sub_data = Template('| $name | $description |')
-connectors_data = Template(
-    '## Connector\n\n| Name | Description |\n| :- | :- |\n$connectors_sub_data')
+connectors_data = Template('## Connector\n\n| Name | Description |\n| :- | :- |\n$connectors_sub_data')
 
 # widget
-widgets_sub_data = Template('| $name | $description |')
-widgets_data = Template(
-    '## Widgets\n\n| Name | Description |\n| :- | :- |\n$widgets_sub_data')
+# widgets_sub_data = Template('| $name | $description |')
+widgets_data = Template('## Widgets\n\n| Name | Description |\n| :- | :- |\n$widgets_sub_data')
 
 # playbooks
 playbooks_sub_sub_data = Template('| $name | $description |')
-playbooks_sub_data = Template(
-    '| $name |\n|:------------:|\n\n| Playbook Name | Description |\n| :- | :- |\n$playbooks_sub_sub_data\n')
+playbooks_sub_data = Template('| $name |\n|:------------:|\n\n| Playbook Name | Description |\n| :- | :- |\n$playbooks_sub_sub_data\n')
 playbooks_data = Template('## Playbook Collection\n\n$playbooks_sub_data')
 
 # playbook blocks
 playbook_blocks_sub_data = Template('| $name | $description |')
-playbook_blocks_data = Template(
-    '## Reference Blocks\n\n| Name | Description |\n| :- | :- |\n$playbook_blocks_sub_data')
+playbook_blocks_data = Template('## Reference Blocks\n\n| Name | Description |\n| :- | :- |\n$playbook_blocks_sub_data')
 
 # rules
-rules_sub_data = Template('| $name | $description |')
-rules_data = Template(
-    '## Rules\n\n| Name | Description |\n| :- | :- |\n$rules_sub_data')
+# rules_sub_data = Template('| $name | $description |')
+rules_data = Template('## Rules\n\n| Name | Description |\n| :- | :- |\n$rules_sub_data')
 
 # navigations
-navigations_sub_data = Template('| $name |')
-navigations_data = Template(
-    '## System View\n\n| Name |\n| :- |\n$navigations_sub_data')
+# navigations_sub_data = Template('| $name |')
+navigations_data = Template('## System View\n\n| Name |\n| :- |\n$navigations_sub_data')
 
 # contents
 content = Template('| [Home](../$readme_file) |\n | -------------------------------------------- |\n\n  # Contents\n\nThe **$solution_name** solution pack contains the following resources.\n\n$dashboards_data\n$picklists_data\n$modules_data\n$connectors_data\n$widgets_data\n$global_variables_data\n$roles_data\n$report_head\n$record_sets_data\n$rules_data\n$playbooks_data\n$playbook_blocks_data\n$navigations_data\n\n>**Warning:** We recommend that you clone these playbooks before customizing to avoid loss of information while upgrading the solution pack.\n\n# Next Steps\n| [Installation](./setup.md#installation) | [Configuration](./setup.md#configuration) | [Usage](./usage.md) |\n| ----------------------------------------- | ------------------------------------------- | --------------------- |')
@@ -78,13 +74,11 @@ prerequisites_sub_data = Template('| $spname | $version | $purpose |')
 prerequisites_data = Template('## Prerequisites\nThe **$solution_name** solution pack depends on the following solution packs that are installed automatically &ndash; if not already installed.\n| Solution Pack Name | Version | Purpose |\n| :--------------------- | :---------------------| :--------------------------------------- |\n$prerequisites_sub_data\n\n')
 
 # setup
-setup_connectors_data = Template(
-    '>* **$connector_name** - $connector_description. To configure and use the $connector_name connector, refer to [Configuring $connector_name]($connector_doc_link)')
+setup_connectors_data = Template('>* **$connector_name** - $connector_description. To configure and use the $connector_name connector, refer to [Configuring $connector_name]($connector_doc_link)')
 setup = Template('[Home](../$readme_file) |\n|--------------------------------------------|\n\n# Installation\n\n1. To install a solution pack, click **Content Hub** > **Discover**.\n2. From the list of solution pack that appears, search for and select **$solution_name**.\n3. Click the **$solution_name** solution pack card.\n4. Click **Install** on the lower part of the screen to begin the installation.\n\n$prerequisites_data\n# Configuration\nFor optimal performance of **$solution_name** solution pack, you can install and configure the connectors that help with the following:\n\n$setup_connectors_data\n\n# Next Steps\n| [Usage](./usage.md) | [Contents](./contents.md) |\n|---------------------|---------------------------|')
 
 # usage
 usage = Template('[Home](../$readme_file) |\n | -------------------------------------------- |\n\n# Usage\n\nRefer to [Simulate Scenario documentation](https://github.com/fortinet-fortisoar/solution-pack-soc-simulator/blob/develop/docs/usage.md) to understand how to simulate and reset scenarios.\n\nTo understand the process FortiSOAR follows to respond to <SP Specific threat>. We have included the following scenario with this solution pack:\n- Scenario 1\n- Scenario 2\n\nRefer to the subsequent sections to understand how this solution pack\'s automation addresses your needs.\n\n## Scenario 1\n\n## Scenario 2\n\n# Next Steps\n| [Installation](./setup.md#installation) | [Configuration](./setup.md#configuration) | [Contents](./contents.md) |\n| ----------------------------------------- | ------------------------------------------- | --------------------------- |')
 
 # release note
-release_note = Template(
-    '# What\'s New\n\n## Enhancements\n\n## Resolved Issues')
+release_note = Template('# What\'s New\n\n## Enhancements\n\n## Resolved Issues')
